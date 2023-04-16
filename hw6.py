63f499e294a7fd155e56d1979c6479c9503b79f7
da2ai <127407114+da2ai@users.noreply.github.com>
2023-04-17 03:57:20 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,11 @@
+for j in range(1,10):
+  for i in range(2,6):
+      print(f'{i} x{j:2d} = {j*i:2d}',end='\t')
+  print()
+    
+print('\n')
+
+for j in range(1,10):
+  for i in range(6,10):
+      print(f'{i} x{j:2d} = {j*i:2d}',end='\t')
+  print()
